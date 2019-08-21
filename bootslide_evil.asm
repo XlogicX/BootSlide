40236ee40f61cdf1bc6c41e498e9e88dd94951c7
XlogicX <no.axiom@gmail.com>
2019-08-21 16:54:13 -0400

    Create bootslide_evil.asm

---
+++ bootslide_evil.asm
@@ -0,0 +1,273 @@
+;15 Number Slider Game
+;  * Slide the numbers around until they are in order
+;  * This is boot sector ( <512 bytes) game
+;  * Controls to move are up/down/left/right
+;  * Any other key rescrambles numbers
+; Some Fuckery:
+; Polymorphic
+; Abuse of "Call"/Ret
+;   * There are routines to move the tiles in the up/down/left/right direction,
+;     They are controlled by the user AND the scrambling routines. There is a
+;     'valid' way to conditionally call and then return, but this requires a
+;     bit more space with all of the control/flow logic. I would rather just
+;     manually set the corresponding return points once, and then just do a 
+;     bunch of conditional jumps, sort of evil, but less code.
+
+[ORG 0x7c00]
+LEFT EQU 75
+RIGHT EQU 77
+UP EQU 72
+DOWN EQU 80
+
+;Init the environment
+xor ax, ax                ; make it zero
+mov ds, ax                ; DS=0
+mov ss, ax                ; stack starts at 0
+mov sp, 0x9c00            ; 200h past code start
+mov ah, 0xb8              ; text video memory
+mov es, ax                ; ES=0xB800
+mov al, 0x03
+int 0x10
+mov ah, 1
+mov ch, 0x26
+int 0x10
+;Fill in all black
+mov cx, 0x07d0            ; whole screens worth
+cbw                       ; clear ax (black on black with null char)
+xor di, di                ; first coordinate of video mem
+rep stosw                 ; push it to video memory
+
+;draw border
+mov di, 1 * 160 + 44                   ; corner to start on
+mov ah, 0xcc                           ; light-red
+mov bx, 22                             ; rows
+mov word [rectrow + 1], 30             ; columns
+mov word [nextline + 2], 160 - (30*2)  ; put column number before '*2'
+call drawrect
+
+call loadtiles                         ; Unpack and load ordered tile numbers into memory
+
+; Scramble
+mov cl, 0xff              ; Start from solved board and do 255 random moves
+scramble:
+dec cx
+je gameloop               ; Once done, go to main game loop
+rdtsc                     ; Get 'random' value
+and al, 3                 ; Only preserve last 2 bits (for 4 possible up/down/left/right moves)
+push word scramble        ; point of return instead of using call for the below jumps
+; Do a random tile move based on random results
+cmp al, 0
+je up
+cmp al, 1
+je down
+cmp al, 2
+je left
+cmp al, 3
+je right
+
+; The Main Game Loop
+gameloop:
+  call drawboard          ; Draw the blank tiles of the board
+  waitkey:
+  call displaytiles       ; Put the actual (hex) numbers into the tiles
+  mov ah, 1               ; Is there a key
+  int 0x16                ; ""
+  jz waitkey              ; If not wait for a key
+  cbw                     ; clear ax (Get the key)
+  int 0x16                ; ""
+  push word gameloop      ; point of return instead of using call
+  ; Get the Keys
+  cmp ah, UP
+  je up
+  cmp ah, DOWN
+  je down
+  cmp ah, LEFT
+  je left
+  cmp ah, RIGHT
+  je right
+  int 0x19                ; A non-directional key was pressed (reboot/rescramble)
+  up:
+    mov bx, [blanklocation]      ; get blank tile location
+    cmp bx, 0xb                  ; Out of bounds?
+    ja keysdone                  ; Then return (don't move anything)
+    add bx, 4                    ; get location above it
+    mov al, [boarddata + bx]     ; get value above it
+    mov byte [boarddata + bx], 0 ; make it the new blank
+    mov [blanklocation], bx      ; update blank location
+    sub bx, 4                    ; revert to old blank location
+    mov [boarddata + bx], al     ; put new value in
+    ret
+  down:
+    mov bx, [blanklocation]      ; get blank tile location
+    cmp bx, 4                    ; Out of bounds?
+    jb keysdone                  ; Then return (don't move anything)
+    sub bx, 4                    ; get location above it
+    mov al, [boarddata + bx]     ; get value above it
+    mov byte [boarddata + bx], 0 ; make it the new blank
+    mov [blanklocation], bx      ; update blank location
+    add bx, 4                    ; revert to old blank location
+    mov [boarddata + bx], al     ; put new value in
+    ret
+  left:
+    mov bx, [blanklocation]      ; get blank tile location
+    cmp bl, 3                    ; All the way to the left?
+    je keysdone                  ; return without moving anything
+    cmp bl, 7                    ; ""
+    je keysdone                  ; ""
+    cmp bl, 11                   ; ""
+    je keysdone                  ; ""
+    cmp bl, 15                   ; ""
+    je keysdone                  ; ""
+    add bl, 1                    ; get location above it
+    mov al, [boarddata + bx]     ; get value above it
+    mov byte [boarddata + bx], 0 ; make it the new blank
+    mov [blanklocation], bx      ; update blank location
+    sub bl, 1                    ; revert to old blank location
+    mov [boarddata + bx], al     ; put new value in
+    ret 
+  right:
+    mov bx, [blanklocation]      ; get blank tile location
+    ; Test Right edge, this is aligned with positions 0, 4, 8, and 12.
+    ; In binary, these are the only positions where the least significant
+    ; bits are always 0, so the below instructions tests for this quality
+    test bl, 0x1                 ; is the last bit a 1
+    jne right_cont               ; if not (0), continue with movement
+    test bl, 0x2                 ; is the next bit a 1
+    je keysdone                  ; if so, don't move anything ... somehow this is the right logic
+    right_cont:
+    sub bl, 1                    ; get location above it
+    mov al, [boarddata + bx]     ; get value above it
+    mov byte [boarddata + bx], 0 ; make it the new blank
+    mov [blanklocation], bx      ; update blank location
+    add bl, 1                    ; revert to old blank location
+    mov [boarddata + bx], al     ; put new value in 
+    ret
+  keysdone:
+  ret
+
+displaytiles:
+; This is a routine for displaying all of the tiles
+  xor bx, bx
+  mov di, 4 * 160 + 52    ; Position of top-left tile
+  displaytilesloop:
+    mov ah, 0x40          ; Black on red background
+    mov al, byte [boarddata + bx]  ; current number value in boarddata structure
+    inc bx                ; next tile digit for next iteration
+    add al, 0x30          ; 'Convert' to ASCII
+    cmp al, 0x3A          ; Is it above '9'
+    jb number             ; If its below (in range of number), skip adjustment
+      add al, 7           ; Get it into the ASCII A-F range
+    number:    
+    cmp al, 0x30          ; Check to see if it's 0 (our blank tile)
+    jne next_a            ; Keep processing if it's not
+    call blanktile        ; Otherwise, draw a blank (black) tile
+    jmp next_b            ; Skip over normal printing of number
+
+    next_a:
+    stosw                 ; Print the Number
+
+    next_b:
+    add di, 12            ; Go to the next column
+    cmp bx, 4             ; Check if it's the last column
+    je nextrow            ; If so, do a row adjustment
+    cmp bx, 8             ; ""
+    je nextrow            ; ""
+    cmp bx, 12            ; ""
+    jne norows            ; ""
+    nextrow:
+      add di, 744         ; Coordinate adjustment for going to next row
+    norows:
+    cmp bx, 0x10          ; check to see if last tile has been printed
+    jne displaytilesloop  ; otherwise keep getting and printing them
+ret
+
+blanktile:
+  ; A Blank Tile for the '0' tile
+  push bx                               ; Don't pave over bx, as other routines us it
+  sub di, 326                           ; Adjust coord from middle to upper-left corner of tile
+  cbw                                   ; clear ax (black)
+  mov bx, 5                             ; rows
+  mov word [rectrow + 1], 7             ; columns
+  mov word [nextline + 2], 160 - (7*2)  ; put column number before '*2'
+  call drawrect                         ; Draw the black rectangle
+  sub di, 472                           ; Restore coordinate to middle of tile
+  pop bx                                ; Restore bx
+ret
+
+loadtiles:
+; This is the unpacking routine, it takes the number data (a nibble for each number)
+; and unpacks it out to bytes of data in memory space after the loaded program (0x7e00)
+  xor bx, bx
+  xor si, si
+  mov cx, 8                   ; 8 bytes to process
+  tilesloop:
+    mov al, [ordered + bx]    ; Get packed byte
+    and al, 0x0f              ; mask out first nibble
+    mov [boarddata + si], al  ; put it into memory
+    inc si                    ; increment to next byte of memory
+    mov al, [ordered + bx]    ; Get same packed byte
+    and al, 0xf0              ; Now get upper nibble
+    shr al, 4                 ; shift it down
+    mov [boarddata + si], al  ; put it into memory
+    inc si                    ; increment to next byte of memory
+    inc bx                    ; increment to next packed byte
+    loop tilesloop
+ret
+
+drawboard:
+; This routine graphically draws the tiles (without the numbers)
+  mov dx, 2 * 160 + 46 - 758  ; This is much before the start position, helps keep a tighter loop
+  mov si, 4                   ; 4 rows
+  row:
+    add dx, 758               ; Advance to next row
+    call drawtile
+    add dx, 14                ; next column
+    call drawtile
+    add dx, 14
+    call drawtile
+    add dx, 14
+    call drawtile
+    dec si
+    jne row
+ret
+
+drawtile:
+; Color 1
+mov di, dx
+mov ax, 0x48b1                        ; shaded corner border
+mov bx, 5                             ; rows
+mov word [rectrow + 1], 7             ; columns
+mov word [nextline + 2], 160 - (7*2)  ; put column number before '*2'
+call drawrect
+; Color 2
+mov di, dx
+mov ah, 0x44                          ; inner light blue main part of tile
+mov bx, 4                             ; rows
+mov word [rectrow + 1], 6             ; columns
+mov word [nextline + 2], 160 - (6*2)  ; put column number before '*2'
+call drawrect
+ret
+
+drawrect:
+; Draws rectangle
+rectrow: mov cx, 0         ; columns, self-modified by caller
+rep stosw
+; add di, (next line value) (ammount self-modified by caller)
+nextline: db 0x81, 0xc7, 0x00, 0x00
+dec bx
+jne rectrow
+ret
+
+; Data structure for ordered game
+ordered:
+dw 0xcdef, 0x89ab, 0x4567, 0x0123
+
+blanklocation:
+db 0xf
+
+;BIOS sig and padding
+times 510-($-$$) db 0
+dw 0xAA55
+
+boarddata:
+; 16 bytes of board data starting at 0x7e00
